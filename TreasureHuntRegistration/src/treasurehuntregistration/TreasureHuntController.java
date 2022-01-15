df8cd11362ed2c63bd52e171fd88ac36cc39ca9b
wanifadzil <syazwanifadzil12@gmail.com>
2022-01-15 16:55:23 +0800

    added ListOverviewGames and ListGameRegistered method

--- TreasureHuntRegistration/src/treasurehuntregistration/TreasureHuntController.java
+++ TreasureHuntRegistration/src/treasurehuntregistration/TreasureHuntController.java
@@ -2,15 +2,42 @@ package treasurehuntregistration;
 
 public class TreasureHuntController {
 
+    FileStore fs = FileStore.getInstance();
+
     //TODO: Wani
     public void listOverviewGames(){
         // Display Game database
+//        System.out.println("\n------------------");
+//        System.out.println(" AVAILABLE GAMES");
+//        System.out.println("--------------------\n");
+//        System.out.println("----------------------------------------------------------------");
+//        System.out.println("No.\t\tGame\t\tMax Player\tCurrent player");
+//        System.out.println("----------------------------------------------------------------\n");
+        for (Game i : fs.getGameList().values()) {
+            System.out.println(i.id+ "\t\t"+ i.name + "\t\t" + i.maxOfPlayers+ "\t\t"+ i.currentRegisteredPlayers);
+        }
     }
 
     //TODO: Wani
     public void listGamesRegistered(){
         // Display player database
-
+//        System.out.println("\n------------------");
+//        System.out.println(" AVAILABLE PLAYERS");
+//        System.out.println("--------------------\n");
+//        System.out.println("----------------------------------------------------------------");
+//        System.out.println("Players\t\tGame Joined");
+//        System.out.println("----------------------------------------------------------------\n");
+        for (Player i : fs.getPlayers().values()) {
+            System.out.print(i.id + "\t\t[ ");
+            for(int j = 0 ; j < i.gamesJoined.length; j++){
+                for (Game k : fs.getGameList().values()) {
+                    if(k.id == i.gamesJoined[j]){
+                        System.out.print(k.name+ " ");
+                    }
+                }
+            }
+            System.out.println("]");
+        }
     }
 
     //TODO: Tom
